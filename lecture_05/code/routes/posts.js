a468f7820423ca50829b086a7a0661aa5348d02a
Leena Domadia <leena.domadia2018@gmail.com>
2022-02-15 23:00:12 -0500

    updated lecture 5 code

--- lecture_05/code/routes/posts.js
+++ lecture_05/code/routes/posts.js
@@ -3,6 +3,37 @@ const router = express.Router();
 const data = require('../data');
 const postData = data.posts;
 
+// New way of doing it:
+// You can link multiple requests in same block of code
+router.route('/').get(async (req, res) => {
+  try {
+    const postList = await postData.getAllPosts();
+    res.json(postList);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+}).post(async (req, res) => {
+  res.send('POST request to http://localhost:3000/posts')
+}).delete(async (req, res) => {
+  res.send('DELETE request to http://localhost:3000/posts')
+});
+
+router.route('/:id').get(async (req, res) => {
+  try {
+    req.params.id = validation.checkId(req.params.id);
+    const post = await postData.getPostById(req.params.id);
+    res.json(post);
+  } catch (e) {
+    res.status(404).json(e);
+  }
+}).post(async (req, res) => {
+  res.send(`POST request to http://localhost:3000/posts/${req.params.id}`)
+}).delete(async (req, res) => {
+  res.send(`DELETE request to http://localhost:3000/posts/${req.params.id}`)
+});
+
+//----------------------------------------------------------------------
+
 router.get('/:id', async (req, res) => {
   try {
     const post = await postData.getPostById(req.params.id);
--- lecture_05/code/routes/users.js
+++ lecture_05/code/routes/users.js
@@ -3,6 +3,37 @@ const router = express.Router();
 const data = require('../data');
 const userData = data.users;
 
+// The new way
+router.route('/').get(async (req, res) => {
+  try {
+    const userList = await userData.getAllUsers();
+    res.json(userList);
+  } catch (e) {
+    // Something went wrong with the server!
+    res.status(500).send();
+  }
+}).post(async (req, res) => {
+  res.send('POST request to http://localhost:3000/users')
+}).delete(async (req, res) => {
+  res.send('DELETE request to http://localhost:3000/users')
+});
+
+router.route('/:id').get(async (req, res) => {
+  try {
+    req.params.id = validation.checkId(req.params.id);
+    const user = await userData.getPostById(req.params.id);
+    res.json(user);
+  } catch (e) {
+    res.status(404).json(e);
+  }
+}).post(async (req, res) => {
+  res.send(`POST request to http://localhost:3000/users/${req.params.id}`)
+}).delete(async (req, res) => {
+  res.send(`DELETE request to http://localhost:3000/users/${req.params.id}`)
+});
+
+//---------------------------------------------------------------------
+
 router.get('/:id', async (req, res) => {
   try {
     const user = await userData.getUserById(req.params.id);
